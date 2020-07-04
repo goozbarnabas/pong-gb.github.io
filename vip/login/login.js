8dc72fd47dd5d9e601f0bdb36772c5aeaeec6223
goozbarnabas <admin@goozbarnabas.ga>
2020-07-04 10:06:11 +0200

    0.1.2
    Testing login system

---
+++ vip/login/login.js
@@ -0,0 +1,11 @@
+function login() {
+        var username = document.getElementById('username').value
+        var password = document.getElementById('password').value
+
+        var user1 = 'goozbarnabas'
+        var pass1 = 'Barni1234'
+
+        if(username == user1 && password == pass1){
+                window.location = 'https://pong-game.tk/vip/0100.html'
+        }
+}
